de070d091e5d266a487f3071463ca779638b8bab
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-06 15:46:51 +0100

    chore: create non-minified dist file (#3)

--- dist/angular-dailymotion-api-factory.js
+++ dist/angular-dailymotion-api-factory.js
@@ -1,3 +1,10 @@
+/**
+    @name: angular-dailymotion-api-factory 
+    @version: 0.5.0 (06-01-2016) 
+    @author: Jonathan Hornung <jonathan.hornung@gmail.com> 
+    @url: https://github.com/JohnnyTheTank/angular-dailymotion-api-factory#readme 
+    @license: MIT
+*/
 "use strict";
 
 angular.module("jtt_dailymotion", [])
